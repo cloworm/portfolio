21b4e80da1e3070dd144af3ad77a59ef8f9af812
cloworm <chloe.echikson@gmail.com>
2020-12-25 10:04:58 -0500

    replace object map with jsx

--- src/card.tsx
+++ src/card.tsx
@@ -1,45 +1,59 @@
 import React, { ReactElement } from 'react'
 
-import Project from './types/project.type'
 import Chip from './chip'
 import ButtonLink from './button-link'
 
 interface Props {
+  year: number
+  name: string
+  description: string
+  repo: string
+  link: string
+  bgImage: string
+  bgPosition?: string
   flipped: boolean
-  project: Project
+  tags: string[]
 }
 
-const Card = ({ project, flipped }: Props): ReactElement => {
+const Card = ({
+  year,
+  name,
+  description,
+  repo,
+  link,
+  bgImage,
+  bgPosition,
+  flipped,
+  tags,
+}: Props): ReactElement => {
   return (
     <div className="relative rounded-xl group hover:border-transparent hover:shadow-xl shadow transition-all text-left grid lg:grid-cols-2 mb-10 transform hover:scale-105">
       <div className={`${flipped ? 'lg:order-1 lg:rounded-r-xl' : 'lg:rounded-l-xl'} px-8 py-8 bg-white`}>
         <p className="text-sm font-semibold text-theme_pink">
-          {project.year}
+          {year}
         </p>
         <div className="pb-3">
           <div className="float-right space-x-2">
-            <ButtonLink url={project.repo}>Source</ButtonLink>
-            <ButtonLink url={project.link}>Demo</ButtonLink>
+            <ButtonLink url={repo}>Source</ButtonLink>
+            <ButtonLink url={link}>Demo</ButtonLink>
           </div>
           <p className="text-2xl font-bold tracking-wide align-center">
-            {project.name}
+            {name}
           </p>
         </div>
         <p className="pb-10 text-sm">
-          {project.description}
+          {description}
         </p>
-        {project.tags.map((tag, idx) => {
+        {tags.map((tag, idx) => {
           return (
-            <div key={`${project.name}-div${idx}`} className="inline-block">
-              <Chip key={`${project.name}-${idx}`}>{tag}</Chip>
+            <div key={`${name}-div${idx}`} className="inline-block">
+              <Chip key={`${name}-${idx}`}>{tag}</Chip>
             </div>
           )
         })}
-
       </div>
 
-      <div style={{ backgroundImage: project.bgImage }} className={`bg-cover ${project.bgPosition ? project.bgPosition : 'bg-left-top'} bg-no-repeat ${flipped ? 'rounded-l-xl' : 'rounded-r-xl'} flex justify-center border-4 border-white h-40`}>
-      </div>
+      <div style={{ backgroundImage: bgImage }} className={`bg-cover ${bgPosition ? bgPosition : 'bg-left-top'} bg-no-repeat ${flipped ? 'rounded-l-xl' : 'rounded-r-xl'} flex justify-center border-4 border-white h-40`} />
     </div>
   )
 }
